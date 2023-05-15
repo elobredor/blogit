dae694597b3e0c3929f559d9eba48168311974ec
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-15 17:35:53 -0600

    export global for Environment Variables and set mongooseModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MONGODB_URI } from './config/data.source';
 
 @Module({
   imports: [
@@ -9,6 +11,7 @@ import { AppService } from './app.service';
       envFilePath: `.${process.env.NODE_ENV.trim()}.env`,
       isGlobal: true,
     }),
+    MongooseModule.forRoot(MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
