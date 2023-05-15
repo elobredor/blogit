396d7700b930efd9f954723cc1a1c4485514671a
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-15 16:40:45 -0600

    configure morgan, enable cors and set prefix '/api'

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,41 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { CORS } from './constants/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //use morgan
+  app.use(morgan('dev'));
+
+  // Validation pipe for all requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  /* The `Reflector` class is used
+  to retrieve metadata associated with classes, methods, and properties in a NestJS application. */
+  const reflector = app.get(Reflector);
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+  // Get config service
+  const configService = app.get(ConfigService);
+
+  // Enable CORS
+  app.enableCors(CORS);
+
+  // Set prefix
+  app.setGlobalPrefix('api');
+
+  await app.listen(configService.get('PORT'));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
