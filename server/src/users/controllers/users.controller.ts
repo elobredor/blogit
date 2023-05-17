e74088a7fd7b9273c32b5e55fa2ed088260566a6
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-16 20:03:17 -0600

    method to get user by ID

--- server/src/users/controllers/users.controller.ts
+++ server/src/users/controllers/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/user.dto';
 
@@ -13,6 +13,15 @@ export class UsersController {
     return response.status(HttpStatus.CREATED).json({
       message: 'User has been created successfully',
       newUser,
+    });
+  }
+
+  /* This is a method in a NestJS controller that handles a GET request to retrieve a user by their UserID.*/
+  @Get(':id')
+  async getStudent(@Res() response, @Param('id') userId: string) {
+    const userExist = await this.usersService.findOne(userId);
+    return response.status(HttpStatus.OK).json({
+      user: userExist,
     });
   }
 }
