fbdb0d54e25b1225d96fea03361c70499cb74c47
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-18 18:06:22 -0600

    fix comment interface

--- server/src/interfaces/comment.interface.ts
+++ server/src/interfaces/comment.interface.ts
@@ -1,5 +1,8 @@
-export interface Comment {
+import { Document } from 'mongoose';
+
+export interface CommentInterface extends Document {
   readonly commentId: string;
+  readonly postId: string;
   readonly userId: string;
   readonly userName: string;
   readonly comment: string;
