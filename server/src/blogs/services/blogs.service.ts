ed93fac410f49744e148781d1eb6e7dd87c1e1d5
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-18 18:04:25 -0600

    fix function to create a blog

--- server/src/blogs/services/blogs.service.ts
+++ server/src/blogs/services/blogs.service.ts
@@ -4,32 +4,27 @@ import { Model } from 'mongoose';
 import { BlogInterface } from 'src/interfaces/blog.interface';
 import { CreateBlogsDTO } from '../dto/blogs.dto';
 import { ErrorManager } from 'src/utils/error.manager';
-import { UserInterface } from 'src/interfaces/user.interface';
 
 @Injectable()
 export class BlogsService {
-  constructor(
-    @InjectModel('blogs') private readonly blogModel: Model<BlogInterface>,
-    @InjectModel('blogs') private readonly userModel: Model<UserInterface>,
-  ) {}
+  constructor(@InjectModel('blogs') private readonly blogModel: Model<BlogInterface>) {}
 
   //This function creates a new blog in a database using the provided data and returns the created blog.
-  public async createBlog(body: CreateBlogsDTO, userId: string): Promise<BlogInterface> {
+  public async createBlog(body: CreateBlogsDTO): Promise<BlogInterface> {
     try {
-      const user = await this.userModel.findOne({ userId });
+      const { userId, category } = body;
+      const blogExist = await this.blogModel.findOne({ userId, category });
 
-      //If the user is not found, throw an error.
-      if (!user) {
+      //If the blog already exists, throw an error.
+      if (blogExist?.userId === userId && blogExist?.category === category) {
         throw new ErrorManager({
-          type: 'NOT_FOUND',
-          message: `User ID: ${userId} not found`,
+          type: 'FORBIDDEN',
+          message: `Blog with userId: ${userId} and category: ${category} already exists`,
         });
       }
       //If the user is found, create a new blog and save it to the database.
       const newBlog = new this.blogModel(body);
-      //This is a workaround for the bug in mongoose where the _id is not being set.
-      await user.updateOne({ blog: { _id: newBlog._id, category: body.category } });
-      return;
+      return await newBlog.save();
     } catch (error) {
       throw ErrorManager.createSignatureError(error.message);
     }
