9824ad212bddacc998ffffbaa5a55683c7562381
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-18 18:00:40 -0600

    add blog, post and comments modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MONGODB_URI } from './config/data.source';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BlogsModule } from './blogs/blogs.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -16,8 +16,8 @@ import { BlogsModule } from './blogs/blogs.module';
     MongooseModule.forRoot(MONGODB_URI),
     UsersModule,
     BlogsModule,
+    PostsModule,
+    CommentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
