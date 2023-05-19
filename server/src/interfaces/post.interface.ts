d1b707c18c80a08560fad83abf0f2a2e48ae01bf
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-18 18:06:38 -0600

    fix posts interface

--- server/src/interfaces/post.interface.ts
+++ server/src/interfaces/post.interface.ts
@@ -1,14 +1,13 @@
-import { Comment } from './comment.interface';
+import { Document } from 'mongoose';
 
-export interface Post {
+export interface PostsInterface extends Document {
   readonly postId: string;
+  readonly blogId: string;
   readonly title: string;
   readonly content: string;
   readonly images: string[];
   readonly createdAt: Date;
   readonly updatedAt: Date;
   readonly status: number;
-  readonly comments: Comment[];
   readonly postLikes: string[];
-  readonly savedPosts: string[];
 }
