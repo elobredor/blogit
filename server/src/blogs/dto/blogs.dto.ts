5293e0a577f841470973416a93084b32f245ca8e
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-18 18:03:41 -0600

    add userId to DTO

--- server/src/blogs/dto/blogs.dto.ts
+++ server/src/blogs/dto/blogs.dto.ts
@@ -3,5 +3,9 @@ import { IsString, MaxLength } from 'class-validator';
 export class CreateBlogsDTO {
   @IsString()
   @MaxLength(30)
+  readonly userId: string;
+
+  @IsString()
+  @MaxLength(30)
   readonly category: string;
 }
