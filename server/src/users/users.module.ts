30bdc4a5139ad722fa2741ca67480287661a5aa7
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-18 18:14:28 -0600

    fix user module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -2,11 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
-import { BlogSchema } from 'src/schemas/blog.schema';
+import { UserSchema } from 'src/schemas/users.schema';
 
 @Global()
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'blogs', schema: BlogSchema }])],
+  imports: [MongooseModule.forFeature([{ name: 'users', schema: UserSchema }])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
