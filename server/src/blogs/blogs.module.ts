fe9f86aec07897f5277a2a3e6fb7dd2991ff292c
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-18 18:01:45 -0600

    remove userService from blogModule

--- server/src/blogs/blogs.module.ts
+++ server/src/blogs/blogs.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogsService } from './services/blogs.service';
 import { BlogsController } from './controllers/blogs.controller';
-import { BlogSchema } from 'src/schemas/blog.schema';
-import { UsersService } from 'src/users/services/users.service';
+import { BlogSchema } from 'src/schemas/blogs.schema';
 
+@Global()
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'blogs', schema: BlogSchema }])],
   controllers: [BlogsController],
-  providers: [BlogsService, UsersService],
+  providers: [BlogsService],
 })
 export class BlogsModule {}
