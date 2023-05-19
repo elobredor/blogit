e3f5c01eab0ec91d3492b03f35fe7564bf569135
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-19 12:14:16 -0600

    create functions to getAllPosts, postByID, add Like

--- server/src/posts/controllers/posts.controller.ts
+++ server/src/posts/controllers/posts.controller.ts
@@ -1,17 +1,47 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
 import { PostsService } from '../services/posts.service';
 import { CreatePostsDTO } from '../dto/posts.dto';
+import { ParseObjectIdPipe } from 'src/utils/parse-object-id-pipe.pipe';
+import { CreatePostsLikesDTO } from '../dto/postLikes.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  //function
+  //function to register a new post
   @Post('create')
-  public async createBlog(@Res() response, @Body() body: CreatePostsDTO) {
+  public async createPost(@Res() response, @Body() body: CreatePostsDTO) {
     await this.postsService.createPost(body);
     return response.status(HttpStatus.CREATED).json({
       message: 'Post has been created successfully',
+    });
+  }
+  //function to get all posts order by date
+  @Get('all')
+  public async getAllPosts(@Res() response, @Param('page') page: number) {
+    const posts = await this.postsService.getAllPosts(page);
+    return response.status(HttpStatus.OK).json({
+      posts,
+    });
+  }
+  //function to get a single post
+  @Get(':postId')
+  public async getSinglePost(@Res() response, @Param('postId', ParseObjectIdPipe) postId: string) {
+    const post = await this.postsService.getPostById(postId);
+    return response.status(HttpStatus.OK).json({
+      post,
+    });
+  }
+  //Like/disLike a post
+  @Put('like/:postId')
+  public async likePost(
+    @Res() response,
+    @Body() body: CreatePostsLikesDTO,
+    @Param('postId', ParseObjectIdPipe) postId: string,
+  ) {
+    await this.postsService.addLike(body, postId);
+    return response.status(HttpStatus.OK).json({
+      message: 'Post has been updated successfully',
     });
   }
 }
