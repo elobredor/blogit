abf3237df65a22e85384f6790fa6f1a40df9247b
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-19 16:12:31 -0600

    remove property readonly

--- server/src/interfaces/comment.interface.ts
+++ server/src/interfaces/comment.interface.ts
@@ -6,7 +6,7 @@ export interface CommentInterface extends Document {
   readonly userId: string;
   readonly userName: string;
   readonly comment: string;
-  readonly commentLikes: string[];
+  commentLikes: string[];
   readonly createdAt: Date;
   readonly updatedAt: Date;
   readonly status: number;
