dd077ce5694e5f717de52731d75b225b8fc146f4
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-19 18:51:26 -0600

    validate if the user exist and the category is no the same

--- server/src/blogs/services/blogs.service.ts
+++ server/src/blogs/services/blogs.service.ts
@@ -4,19 +4,34 @@ import { Model } from 'mongoose';
 import { BlogInterface } from 'src/interfaces/blog.interface';
 import { CreateBlogsDTO } from '../dto/blogs.dto';
 import { ErrorManager } from 'src/utils/error.manager';
+import { UserInterface } from 'src/interfaces/user.interface';
 
 @Injectable()
 export class BlogsService {
-  constructor(@InjectModel('blogs') private readonly blogModel: Model<BlogInterface>) {}
+  constructor(
+    @InjectModel('blogs') private readonly blogModel: Model<BlogInterface>,
+    @InjectModel('users') private readonly usersModel: Model<UserInterface>,
+  ) {}
 
   //This function creates a new blog in a database using the provided data and returns the created blog.
   public async createBlog(body: CreateBlogsDTO): Promise<BlogInterface> {
     try {
       const { userId, category } = body;
-      const blogExist = await this.blogModel.findOne({ userId, category });
+      const userExist = await this.usersModel.findById(userId);
 
-      //If the blog already exists, throw an error.
-      if (blogExist?.userId === userId && blogExist?.category === category) {
+      //If the user does not exists, throw an error.
+      if (!userExist) {
+        throw new ErrorManager({
+          type: 'FORBIDDEN',
+          message: `User not valid`,
+        });
+      }
+
+      const blogExist = await this.blogModel.find({ userId });
+
+      const categoryExist = blogExist.find((blog) => blog.category === category);
+      //If the category already exists, throw an error.
+      if (categoryExist) {
         throw new ErrorManager({
           type: 'FORBIDDEN',
           message: `Blog with userId: ${userId} and category: ${category} already exists`,
