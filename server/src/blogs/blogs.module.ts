d9f70a3f1e9ae84d28407003c39786e322a0d58b
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-19 18:50:47 -0600

    add userModel

--- server/src/blogs/blogs.module.ts
+++ server/src/blogs/blogs.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BlogsService } from './services/blogs.service';
 import { BlogsController } from './controllers/blogs.controller';
 import { BlogSchema } from 'src/schemas/blogs.schema';
+import { UserSchema } from 'src/schemas/users.schema';
 
 @Global()
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'blogs', schema: BlogSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'blogs', schema: BlogSchema }]),
+    MongooseModule.forFeature([{ name: 'users', schema: UserSchema }]),
+  ],
   controllers: [BlogsController],
   providers: [BlogsService],
 })
