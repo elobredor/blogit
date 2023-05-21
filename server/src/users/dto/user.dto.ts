f75e47100d35bfa73eb6f16fed1cc0ff07cb4f0b
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-20 21:01:40 -0600

    create update user dto

--- server/src/users/dto/user.dto.ts
+++ server/src/users/dto/user.dto.ts
@@ -30,4 +30,66 @@ export class CreateUserDto {
   @IsEmpty({ message: 'El campo no debe tener valor cuando se envía como predeterminado.' })
   @IsNumber()
   status: number;
+
+  @IsOptional()
+  @MaxLength(150)
+  @IsOptional()
+  about: string;
+
+  @IsOptional()
+  @MaxLength(75)
+  @IsString()
+  socialNetwork1: string;
+
+  @IsOptional()
+  @MaxLength(75)
+  @IsString()
+  socialNetwork2: string;
+}
+
+export class UserUpdateDTO {
+  @IsString()
+  @MaxLength(16)
+  @IsOptional()
+  userId: string;
+
+  @IsString()
+  @MaxLength(50)
+  @IsOptional()
+  userName: string;
+
+  @IsString()
+  @MaxLength(40)
+  @IsOptional()
+  email: string;
+
+  @IsString()
+  @MaxLength(70)
+  @IsOptional()
+  profileImage: string;
+
+  @IsOptional()
+  @IsEnum(ROLES)
+  @IsEmpty({ message: 'El campo no debe tener valor cuando se envía como predeterminado.' })
+  role: ROLES;
+
+  @IsOptional()
+  @IsEmpty({ message: 'El campo no debe tener valor cuando se envía como predeterminado.' })
+  @IsNumber()
+  status: number;
+
+  @IsOptional()
+  @MaxLength(150)
+  @IsOptional()
+  about: string;
+
+  @IsOptional()
+  @MaxLength(75)
+  @IsString()
+  socialNetwork1: string;
+
+  @IsOptional()
+  @MaxLength(75)
+  @IsString()
+  socialNetwork2: string;
 }
