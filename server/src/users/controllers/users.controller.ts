8443e4d6196bca3bc1f8db2ed6d4bdd6aaf480d6
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-22 09:38:47 -0600

    add express response

--- server/src/users/controllers/users.controller.ts
+++ server/src/users/controllers/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/user.dto';
 
@@ -8,17 +9,16 @@ export class UsersController {
 
   /* This is a method in a NestJS controller that handles a POST request to create a new user. */
   @Post('create')
-  public async create(@Res() response, @Body() body: CreateUserDto) {
+  public async create(@Res() response: Response, @Body() body: CreateUserDto) {
     const newUser = await this.usersService.create(body);
     return response.status(HttpStatus.CREATED).json({
       message: 'User has been created successfully',
       newUser,
     });
   }
-
   /* This is a method in a NestJS controller that handles a GET request to retrieve a user by their UserID.*/
   @Get(':userId')
-  public async getStudent(@Res() response, @Param('userId') userId: string) {
+  public async getStudent(@Res() response: Response, @Param('userId') userId: string) {
     const userExist = await this.usersService.findOne(userId);
     return response.status(HttpStatus.OK).json({
       user: userExist,
@@ -26,7 +26,7 @@ export class UsersController {
   }
   /* This is a method in a NestJS controller that handles a GET request to retrieve usersProfile.*/
   @Get('profile/:userId')
-  public async getProfile(@Res() response, @Param('userId') userId: string) {
+  public async getProfile(@Res() response: Response, @Param('userId') userId: string) {
     const usersProfile = await this.usersService.getUserProfile(userId);
     return response.status(HttpStatus.OK).json({
       usersProfile,
@@ -34,7 +34,7 @@ export class UsersController {
   }
   //this is a method in NestJs controller that update a user
   @Put('update/:userId')
-  public async updateUser(@Res() response, @Param('userId') userId: string, @Body() body: CreateUserDto) {
+  public async updateUser(@Res() response: Response, @Param('userId') userId: string, @Body() body: CreateUserDto) {
     const updatedUser = await this.usersService.updateUser(userId, body);
     return response.status(HttpStatus.OK).json({
       message: 'User has been successfully updated',
