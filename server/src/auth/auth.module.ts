97f386458138116043672526e72b3a4449c645d6
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-25 14:30:03 -0600

    add schemas to modules

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,9 +5,16 @@ import { UsersService } from 'src/users/services/users.service';
 import { UsersModule } from 'src/users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/schemas/users.schema';
+import { BlogSchema } from 'src/schemas/blogs.schema';
+import { PostSchema } from 'src/schemas/posts.schema';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forFeature([{ name: 'users', schema: UserSchema }])],
+  imports: [
+    UsersModule,
+    MongooseModule.forFeature([{ name: 'users', schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: 'blogs', schema: BlogSchema }]),
+    MongooseModule.forFeature([{ name: 'posts', schema: PostSchema }]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UsersService],
 })
