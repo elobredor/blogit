064a35b9b3f2f10ef08e092a651489c1c4e92a16
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-25 09:25:22 -0600

    fix schema

--- server/src/schemas/users.schema.ts
+++ server/src/schemas/users.schema.ts
@@ -33,8 +33,8 @@ export class User {
   @Prop({ type: String })
   socialNetwork2: string;
 
-  @Prop([{ title: String, posts: [String] }])
-  saved: { title: string; posts: string[] }[];
+  @Prop([{ title: String, posts: [{ postId: String, images: String }] }])
+  saved: { title: string; posts: object[] }[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
