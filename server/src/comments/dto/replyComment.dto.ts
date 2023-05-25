8b93f9032d621f05ea0bf1e1314c54b389b9ae23
Diego-Fdez <dieghoxcr7@gmail.com>
2023-05-25 17:27:25 -0600

    add updateReplyDTO

--- server/src/comments/dto/replyComment.dto.ts
+++ server/src/comments/dto/replyComment.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, MaxLength } from 'class-validator';
+import { IsEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreateReplyCommentDTO {
   @IsString()
@@ -26,4 +26,28 @@ export class CreateReplyCommentLikesDTO {
   @IsString()
   @MaxLength(30)
   replyCommentId: string;
+}
+
+export class UpdateReplyCommentDTO {
+  @IsString()
+  @MaxLength(30)
+  @IsOptional()
+  @IsEmpty({ message: 'El campo userId no debe tener valor cuando se envía como predeterminado.' })
+  userId: string;
+
+  @IsString()
+  @MaxLength(70)
+  @IsOptional()
+  @IsEmpty({ message: 'El campo userName no debe tener valor cuando se envía como predeterminado.' })
+  userName: string;
+
+  @IsString()
+  @MaxLength(250)
+  @IsOptional()
+  @IsEmpty({ message: 'El campo profileImage no debe tener valor cuando se envía como predeterminado.' })
+  profileImage: string;
+
+  @IsString()
+  @MaxLength(1000)
+  comment: string;
 }
