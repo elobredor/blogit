232900980bae10932030ce4017cdd1ce34808e66
Juan José Rodriguez <zoranbow@gmail.com>
2023-05-28 01:17:07 -0500

    feat(comment/article screens)

--- client/src/component/shared/ModalLogin.jsx
+++ client/src/component/shared/ModalLogin.jsx
@@ -15,6 +15,18 @@ import {
   NO_COUNTRY,
 } from 'react-native-dotenv';
 
+// MY_IP=192.168.1.8
+// ALECTO= 6sdfsdjfg88fg8
+// CHRONOS64=dg9tjh7a5fdjg9
+// VITAE=gsdfg8s6d5g7g8h9sdf7
+// ARIADNA=9f8g63fjg9sad0vmn9
+// MEGA_BEATS=7fg8h9a8df7df7hgfghjkjk0l
+// TENBU_HORIN=8gh987dfg6ghh8899ds
+// YEMONJA=sdfg89fgh8gfhj9sdfg8
+// NORVEGICUS=aghgfsdfg8dfg7
+// DIGITAL_DREAMER=g9df8h7gh6k5fg4sdf7
+// NO_COUNTRY=asd7sdfg8dfg7
+
 export function ModalLogin({ modalVisibility, setModalVisibility }) {
   const dispatch = useDispatch();
 
